1657504e769808e585b623d6d35f6ffeea32b1a9
hehemora <mitiyshug@gmail.com>
2024-08-28 19:28:02 +0300

    Delete app/controllers directory

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-    add_flash_types :info, :error, :success
-    def is_editor
-        if @current_user.role_id == Role.find_by(rolename: "editor").id
-            return true
-        end
-    end
-end
