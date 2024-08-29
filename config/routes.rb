a1bcac2a07b27692f0a07bfe085ddc7820dba6c4
hehemora <mitiyshug@gmail.com>
2024-08-29 18:34:10 +0300

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,33 @@
+Rails.application.routes.draw do
+  root "pages#login_page"
+
+  get "/main_page", to: "pages#main_page"
+  get "/cms/cms_page", to: "cms/pages#cms_page"
+  
+  resources :users
+  namespace :cms do
+    resources :users do
+      get :index, on: :member
+      patch :update, on: :member
+    end
+  end
+  get "/auth", to: "sessions#new"
+  post "/auth", to: "sessions#create"
+  get "/quit", to: "sessions#destroy"
+
+  namespace :api do
+    get "articles", to: "articles#index"
+    get "article/:id", to: "articles#show"
+    get "article", to: "articles#show"
+  end
+
+  resources :articles 
+  namespace :cms do
+    resources :articles do
+      patch :publish, on: :member
+    end
+  end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
