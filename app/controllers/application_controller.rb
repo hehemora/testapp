022308f3e1c4e28430358ab54e08ac72391ace8d
hehemora <mitiyshug@gmail.com>
2024-09-11 12:43:52 +0300

    Delete app/controllers directory

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-    add_flash_types :info, :error, :success
-    def current_user
-        if helpers.current_user.blank?
-            return render plain: '401 Unauthorized', status: :unauthorized
-        end
-    end
-end
--- app/controllers/cms/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-module Cms
-  class PagesController < CmsController
-      before_action :is_editor, only: [:cms_page]
-      def cms_page
-          @articles = Article.paginate(page: params[:page], per_page: 10)
-      end
-  end
-end
