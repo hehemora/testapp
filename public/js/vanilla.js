b5185010ae71cea6488c34391966d47c5f624a91
User User <user@vmware.localdomain>
2024-09-25 18:41:33 +0300

    Commit-2509

--- public/js/vanilla.js
+++ public/js/vanilla.js
@@ -1,3 +1,5 @@
+/* Раскрывающийся список */
+
 const dropdown = document.querySelector('.dropdown-content');
 const dropdownBtn = document.querySelector('.dropdown-btn');
 
@@ -8,4 +10,24 @@ dropdownBtn.addEventListener('click', () => {
     else {
         dropdown.classList.add('disp');
     }
+});
+
+/* Отобразить модальное окно */
+document.getElementById("modal-open-btn").addEventListener("click", function() {
+    document.getElementById("modal-window").classList.add("open")
+});
+
+/* Скрыть модальное окно */
+document.getElementById("close-modal-btn").addEventListener("click", function() {
+    document.getElementById("modal-window").classList.remove("open")
+});
+
+/* Отобразить всплывающее меню */
+document.getElementById("materials-btn").addEventListener("click", function() {
+    document.getElementById("materials-list").classList.add("open")
+});
+
+/* Скрыть всплывающее меню */
+document.getElementById("materials-close").addEventListener("click", function() {
+    document.getElementById("materials-list").classList.remove("open")
 });
