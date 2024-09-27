276bd17afa090adfe13d75d3408c603a1683b19e
hehemora <mitiyshug@gmail.com>
2024-09-27 13:31:13 +0300

    Add files via upload

---
+++ app/controllers/cms/pages_controller.rb
@@ -0,0 +1,12 @@
+module Cms
+  class PagesController < CmsController
+      # before_action :is_editor, only: [:cms_page]
+      def cms_page
+          @articles = Article.paginate(page: params[:page], per_page: 10)
+      end
+      
+      def main_page
+        @articles = Article.where(article_statuses_id: 2).paginate(page: params[:page], per_page: 10)
+      end
+  end
+end
