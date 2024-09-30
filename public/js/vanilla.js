01d4d7453285fb3052682715ee9ed502e7556665
User User <user@vmware.localdomain>
2024-09-30 15:21:14 +0300

    Script-3009

--- public/js/vanilla.js
+++ public/js/vanilla.js
@@ -1,46 +1,46 @@
 /* Раскрывающийся список */
 
-const dropdown = document.querySelector('.dropdown-content');
-const dropdownBtn = document.querySelector('.dropdown-btn');
+let dropdown = document.querySelector('.dropdown-content');
+let dropdownBtn = document.querySelector('.dropdown-btn');
 
 dropdownBtn.addEventListener('click', () => {
-    if (dropdown.classList.contains('disp') == true) {
-        dropdown.classList.remove('disp');
+    if (dropdown.classList.contains("disp") == true) {
+        dropdown.classList.remove("disp");
     }
     else {
-        dropdown.classList.add('disp');
+        dropdown.classList.add("disp");
     }
 });
 
 /* Отобразить модальное окно */
-document.getElementById("modal-open-btn").addEventListener("click", function() {
+document.getElementById("modal-open-btn").addEventListener('click', function() {
     document.getElementById("modal-window").classList.add("open")
 });
 
 /* Скрыть модальное окно */
-document.getElementById("close-modal-btn").addEventListener("click", function() {
+document.getElementById("close-modal-btn").addEventListener('click', function() {
     document.getElementById("modal-window").classList.remove("open")
 });
 
-document.getElementById("modal-close").addEventListener("click", function(){
+document.getElementById("modal-close").addEventListener('click', function(){
     document.getElementById("modal-window").classList.remove("open")
 });
 
 
 /* Список материалов */
 
-const materialsBtn = document.getElementById("materials-btn")
-const materials = document.getElementById("materials-list")
+let materialsBtn = document.getElementById("materials-btn");
+let materials = document.getElementById("materials-list");
 
 materialsBtn.addEventListener('click', () => {
-    if (materials.classList.contains('open') == true) {
-        materials.classList.remove('open');
+    if (materials.classList.contains("open") == true) {
+        materials.classList.remove("open");
     }
     else {
-        materials.classList.add('open')
+        materials.classList.add("open")
     }
 });
 
 document.getElementById("materials-close").addEventListener('click', function() {
-    document.getElementById("materials-list").classList.remove('open')
+    document.getElementById("materials-list").classList.remove("open")
 });
