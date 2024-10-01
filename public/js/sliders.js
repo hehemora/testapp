9ef565aa20b15b3ba71f2401a24b44ce69a2de69
User User <user@vmware.localdomain>
2024-10-01 23:09:25 +0300

    SomeAdds-0210

--- public/js/sliders.js
+++ public/js/sliders.js
@@ -1,9 +1,10 @@
-let offset = 0;
-let sliderLine = document.querySelector('.cover-slider');
+var offset = 0;
+const sliderLine = document.querySelector('.cover-slider');
+const sliderNews = document.querySelector('.list-news');
 
 document.querySelector('.next-btn').addEventListener('click', function() {
     offset = offset + 1280;
-    if (offset > 3840) {
+    if (offset > 1280) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
@@ -12,7 +13,23 @@ document.querySelector('.next-btn').addEventListener('click', function() {
 document.querySelector('.prev-btn').addEventListener('click', function() {
     offset = offset - 1280;
     if (offset < 0) {
-        offset = 3840;
+        offset = 1280;
     }
     sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.news-next-btn').addEventListener('click', function() {
+    offset = offset + 1280;
+    if (offset > 1280) {
+        offset = 0;
+    }
+    sliderNews.style.left = -offset + 'px';
+});
+
+document.querySelector('.news-prev-btn').addEventListener('click', function() {
+    offset = newsoffset - 1280;
+    if (offset < 0) {
+        offset = 1280;
+    }
+    sliderNews.style.left = -offset + 'px';
 });
